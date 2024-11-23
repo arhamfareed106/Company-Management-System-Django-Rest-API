14919e6613e81a84cb8e6bd2604d5becfca2bac6
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-23 14:08:54 +0500

    Serializer

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from api import serializers
-from rest_framework import viewsets
+from rest_framework import viewsets # type: ignore
 from api.models import Company
 from api.serializers import CompanySerializer
 # Create your views here.
