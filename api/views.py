ef362703537328f9c8e672f87fa999eda27362bd
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-24 18:56:10 +0500

    views

--- api/views.py
+++ api/views.py
@@ -5,9 +5,8 @@ from api.models import Company
 from api.serializers import CompanySerializer
 # Create your views here.
 
-class CompanyViewSet(viewsets.ModelViewSet):
-    queryset=Company.objects.all
-    serializer_class=CompanySerializer
+
+    
     
     
     
