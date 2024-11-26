2e26e7123a28c127dc1df70f6001498d219b90f4
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-26 14:36:35 +0500

    add fronend

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Company, Employee
 
-admin.site.register(Company)
-admin.site.register(Employee)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'about', 'type', 'added_date', 'active')
+    search_fields = ('name', 'location')
+    list_filter = ('type', 'active')
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'address', 'phone', 'about', 'position', 'company')
+    search_fields = ('name', 'email')
+    list_filter = ('position', 'company')
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee, EmployeeAdmin)
--- api/views.py
+++ api/views.py
@@ -1,27 +1,63 @@
 from argparse import Action
-from multiprocessing import context # type: ignore
+from multiprocessing import context
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from rest_framework import viewsets
 from .models import Company, Employee
 from .serializers import CompanySerializer, EmployeeSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.core.paginator import Paginator
+
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    
+    template_name = 'company_list.html'
+    paginate_by = 9
+
+    def get_queryset(self):
+        queryset = Company.objects.all()
+        search = self.request.GET.get('search', '')
+        if search:
+            queryset = queryset.filter(
+                Q(name__icontains=search) |
+                Q(location__icontains=search) |
+                Q(type__icontains=search)
+            )
+        return queryset
+
     @action(detail=True, methods=['get'])
     def employees(self, request, pk=None):
         try:   
-            company=Company.objects.get(pk=pk)
-            emps= Employee.objects.filter(company=company)
-            emps_serializer = EmployeeSerializer(emps, many=True, context ={'request':request})
+            company = Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(company=company)
+            emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
             return Response(emps_serializer.data)
         except Exception as e:
             print(e)
             return Response({
-                'message':'Company might not exist' 
+                'message': 'Company might not exist'
             })
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    template_name = 'employee_list.html'
+    paginate_by = 9
+
+    def get_queryset(self):
+        queryset = Employee.objects.all()
+        search = self.request.GET.get('search', '')
+        position = self.request.GET.get('position', '')
+        
+        if search:
+            queryset = queryset.filter(
+                Q(name__icontains=search) |
+                Q(email__icontains=search) |
+                Q(company__name__icontains=search)
+            )
+        
+        if position:
+            queryset = queryset.filter(position=position)
+            
+        return queryset.select_related('company')
