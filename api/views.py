afa8fca4258b632bce7767b3d713ca97ccf492c2
arhamfareed106 <arhamfareed575@gmail.com>
2024-11-26 14:02:44 +0500

    serializer

--- api/views.py
+++ api/views.py
@@ -11,10 +11,16 @@ class CompanyViewSet(viewsets.ModelViewSet):
     
     @action(detail=True, methods=['get'])
     def employees(self, request, pk=None):
-        company=Company.objects.get(pk=pk)
-        emps= Employee.objects.filter(company=company)
-        emps_serializer = EmployeeSerializer(emps, many=True, context{'request':request})
-        return Response(emps_serializer.data)
+        try:   
+            company=Company.objects.get(pk=pk)
+            emps= Employee.objects.filter(company=company)
+            emps_serializer = EmployeeSerializer(emps, many=True, context ={'request':request})
+            return Response(emps_serializer.data)
+        except Exception as e:
+            print(e)
+            return Response({
+                'message':'Company might not exist' 
+            })
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
